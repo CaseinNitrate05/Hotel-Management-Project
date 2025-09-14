e8638c22907141d56d1f6517c7da97127a853cc7
Yagnatha Konikineni <163710565+CaseinNitrate05@users.noreply.github.com>
2025-09-14 12:19:29 +0530

    Update settings.py

--- HotelProject/settings.py
+++ HotelProject/settings.py
@@ -64,10 +64,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'HotelProject.wsgi.application'
 
-# Database: PostgreSQL via dj_database_url
+# Database
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
